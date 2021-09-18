4b24287f1afb0a399bac77a7e361fd68d7814740
Wanjoo <wjlee@yiu.ac.kr>
2021-09-18 16:02:58 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const port = 5000
+
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://wjlee:cs1134@bp01.crufi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+.then(() => console.log(`Mongo DB Connected...`))
+.catch(err => console.log(err)); 
+
+app.get('/', (req, res) => res.send('Hello, World, Uhaha~~'))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+// mongodb+srv://wjlee:<password>@bp01.crufi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
