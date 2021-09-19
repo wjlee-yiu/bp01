5c7dd04fb6b9a917975966dfa0eb62237e973768
Wanjoo-note <wjlee@yiu.ac.kr>
2021-09-20 00:24:07 +0900

    간신히복구

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
-const express = require('express')
-const app = express()
-const port = 5000
+const express = require('express');
+const app = express();
+const port = 5000;
+//const bodyParser = require('body-parser');
+const config = require('./config/key');
+const { User } = require('./models/User');
+
+////app.use(bodyParser.urlencoded({extended: true}));
+
+//app.use(bodyParser.json());
 
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://wjlee:cs1134@bp01.crufi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+mongoose.connect(config.mongoURI)
 .then(() => console.log(`Mongo DB Connected...`))
 .catch(err => console.log(err)); 
 
-app.get('/', (req, res) => res.send('Hello, World, Uhaha~~'))
+app.get('/', (req, res) => res.send('Hello, World, Uhaha~~'));
+
+app.post('/register', (req, res) => {
+    const user = new User(req.body);
+    user.save((err, userInfo) => {
+        if(err) return res.json({success: false, err});
+        return res.status(200).json({
+            success: true
+        });
+    });
+
+
+
+});
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 // mongodb+srv://wjlee:<password>@bp01.crufi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
